a71e12538b871ccbe58d581852d8a5e71016ed9b
HamInGyu <hfire1207@gmail.com>
2021-05-07 18:00:42 +0900

    feat: React Sass Btn - add color props

--- styling-with-sass/src/App.js
+++ styling-with-sass/src/App.js
@@ -10,6 +10,24 @@ function App() {
         <Button>button</Button>
         <Button size="small">button</Button>
       </div>
+      <div className="buttons">
+        <Button color="gray" size="large">
+          button
+        </Button>
+        <Button color="gray">button</Button>
+        <Button color="gray" size="small">
+          button
+        </Button>
+      </div>
+      <div className="buttons">
+        <Button color="pink" size="large">
+          button
+        </Button>
+        <Button color="pink">button</Button>
+        <Button color="pink" size="small">
+          button
+        </Button>
+      </div>
     </div>
   );
 }
